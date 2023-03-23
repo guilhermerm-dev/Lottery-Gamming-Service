256eb74c783b4568794645a906bd44b8ecad52c9
Guilherme Ramos Mendes <guilhermeramos@MMB025882CPS.local>
2023-03-23 10:32:15 -0300

    Refactoring

--- src/Lottery.Gamming.Domain/Models/LotteryGame.cs
+++ src/Lottery.Gamming.Domain/Models/LotteryGame.cs
@@ -2,35 +2,36 @@ namespace Lottery.Gamming.Domain.Models;
 
 public abstract class LotteryGame
 {
-    protected LotteryGame()
-    {
-        _random = new Random();
-        Numbers = new List<int>();
-    }
+  protected readonly Random _random;
+  protected readonly int _initialNumber = 1;
 
-    protected readonly int _initialNumber = 1;
-    protected abstract int FinalNumber {get; set;}
-    protected abstract int QuantityOfNumbers {get; set;}
-    protected readonly Random _random;
-    public List<int> Numbers { get; }
-    public virtual void DrawGame()
+  protected LotteryGame()
+  {
+    _random = new Random();
+    Numbers = new List<int>();
+  }
+
+  public List<int> Numbers { get; }
+  protected abstract int FinalNumber { get; set; }
+  protected abstract int QuantityOfNumbers { get; set; }
+  public virtual void DrawGame()
+  {
+    for (int i = 0; i < QuantityOfNumbers; i++)
     {
-        for (int i = 0; i < QuantityOfNumbers; i++)
+      int number = _random.Next(_initialNumber, FinalNumber);
+      if (Numbers.Contains(number))
+      {
+        while (Numbers.Contains(number))
         {
-            int number = _random.Next(_initialNumber, FinalNumber);
-            if (Numbers.Contains(number))
-            {
-                while (Numbers.Contains(number))
-                {
-                    number = _random.Next(_initialNumber, FinalNumber);
-                }
-
-                Numbers.Add(number);
-            }
-            else
-            {
-                Numbers.Add(number);
-            }
+          number = _random.Next(_initialNumber, FinalNumber);
         }
+
+        Numbers.Add(number);
+      }
+      else
+      {
+        Numbers.Add(number);
+      }
     }
+  }
 }
